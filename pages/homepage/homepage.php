40a25a9e279f7de559103db5a86628d92405269d
tnnq <0306231146@caothang.edu.vn>
2026-05-07 13:19:09 +0700

    add section 1,2,3

---
+++ pages/homepage/homepage.php
@@ -0,0 +1,334 @@
+<!-- SECTION 1 -->
+<section style="position:relative; height:736px;">
+    <video autoplay muted loop playsinline
+        style="
+            position:absolute;
+            width:100%;
+            height:100%;
+            object-fit:cover;
+        ">
+        <source src="pages/homepage/media/videoplayback.mp4" type="video/mp4">
+    </video>
+
+    <div style="
+        position:relative;
+        z-index:1;
+        color:white;
+        height:100%;
+        display:flex;
+        flex-direction:column;
+        justify-content:center;
+        align-items:center;
+        text-align:center;
+        gap:24px;
+    ">
+        <div style="
+            font-size: 60px; 
+            line-height: 72px;
+            font-weight: 700; 
+            max-width: 907px;
+            letter-spacing: -0.4px;
+        ">
+            An industry leading interactive 3D home configurator
+        </div>
+        <div style="
+            font-size: 18px; 
+            font-weight: 400; 
+            max-width: 442px;
+            line-height: 28px;
+        ">
+            Away Digital Home is an interactive platform built for the realities of modern homebuying.
+        </div>
+        <div style="display: flex; gap: 16px;">
+            <button style="
+                height:48px;
+                width:147px;
+                font-size:16px;
+                line-height: 20px;
+                font-weight:500;
+                background:#1542FF;
+                color:white;
+                border:none;
+            ">
+                Book a demo
+            </button>
+           <button style="
+                height:48px;
+                width:147px;
+                font-size:16px;
+                line-height: 20px;
+                font-weight:500;
+                background:transparent;
+                color:white;
+                border:1px solid white;
+            ">
+                How it works
+            </button>
+        </div>
+    </div>
+</section>
+
+<!-- SECTION 2 -->
+<section style="
+    background: linear-gradient(180deg, #F3F4F6 0%, #FFFFFF 100%);
+    position:relative; 
+    padding: 80px 150px;
+    height: 832px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+">
+    <div style="
+        width: 1140px;
+        height: 672px;
+        gap: 120px;
+        opacity: 1;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    ">
+        <div style="
+            width: 462px;
+            height: 672px;
+            gap: 64px;
+            opacity: 1;
+            display: flex;
+            flex-direction: column;
+        ">
+            <div style="
+                width: 462px;
+                height: 216px;
+                font-weight: 700;
+                font-size: 60px;
+                line-height: 72px;
+                letter-spacing: -0.4px;
+            ">
+                The <span style="color: #1542FF;">next generation</span> of homebuilding
+            </div>
+            <div style="
+                width: 462px;
+                height: 392px;
+                font-weight: 400;
+                font-size: 18px;
+                line-height: 28px;
+
+                display: flex;
+                flex-direction: column;
+                gap: 30px;
+            ">
+                 <div>
+                    The homebuilding industry is evolving quickly. Traditional 2D plans and disconnected tools no longer give homebuilders the efficiency nor the clarity they need.
+                </div>
+
+                <div>
+                    Away Digital Home helps homebuilders meet today’s expectations by transforming every home design, option and finish into interactive, photorealistic 3D experience. We develop seamless digital experiences that help homebuyers visualize, personalize, and feel confident in the home they’re choosing.
+                </div>
+
+                <div>
+                    When buyers clearly understand their options, your team spends less time explaining, and decisions move forward with far more certainty.
+                </div>
+            </div>
+        </div>
+        <div style="
+            display: flex;
+            flex-direction: column;
+            gap: 24px;
+        ">
+            <img src="pages/homepage/media/img1.png" style="
+                width: 558px;
+                height: 324px;
+            "/>
+            <img src="pages/homepage/media/img2.png" style="
+                width: 558px;
+                height: 324px;
+            "/>
+        </div>
+    </div>
+</section>
+
+<!-- SECTION 3 -->
+<section style="
+    background: #FFFFFF;
+    width: 1440px;
+    height: 1019px;
+    padding: 64px 150px;
+    gap: 64px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+">
+    <div style="
+        width: 846px;
+        height: 171px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 19px;
+    ">
+        <div style="
+            width: 568px;
+            height: 96px;
+            font-weight: 700;
+            font-size: 36px;
+            line-height: 48px;
+            letter-spacing: -0.2px;
+            text-align: center;
+        ">
+            Designed to help homebuilders <span style="color: #1542FF;">sell more homes, faster</span></div>
+        <div style="
+            width: 640px;
+            height: 56px;
+            font-weight: 400;
+            font-size: 18px;
+            line-height: 28px;
+            letter-spacing: 0px;
+            text-align: center;
+            color: #262626;
+        ">
+            <div>Away Digital Home helps your team present, communicate home designs and selections more clearly at every stage of the homebuilding journey.</div>
+        </div>   
+    </div>
+    <div style="
+        width: 1140px;
+        height: 508px;
+        display: flex;
+        flex-direction: row;
+        gap: 24px;
+    ">
+        <div style="
+            width: 364px;
+            height: 508px;
+            display: flex;
+            flex-direction: column;
+            gap: 30px;
+        ">
+            <img src="pages/homepage/media/img3.png" style="
+                width: 364px;
+                height: 294px;
+                opacity: 1;
+            ">
+            <div style="
+                display: flex;
+                flex-direction: column;
+                gap: 16px;
+            ">
+                <div style="
+                    width: 255px;
+                    height: 28px;
+                    font-weight: 700;
+                    font-size: 24px;
+                    line-height: 28px;
+                    letter-spacing: 0;
+                    color: #262626;
+                ">
+                    A faster sales process
+                </div>
+                <div style="
+                    width: 359px;
+                    height: 140px;
+                    font-weight: 400;
+                    font-size: 18px;
+                    line-height: 28px;
+                    letter-spacing: 0px;
+                    color: #303030;
+                ">
+                    Give homebuyers a clear, interactive way to visualise their future home, helping your team guide more efficient decision making process and move them through the sales journey with greater confidence.
+                </div>
+            </div>
+        </div>
+         <div style="
+            width: 364px;
+            height: 508px;
+            display: flex;
+            flex-direction: column;
+            gap: 30px;
+        ">
+            <img src="pages/homepage/media/img4.png" style="
+                width: 364px;
+                height: 294px;
+                opacity: 1;
+            ">
+            <div style="
+                display: flex;
+                flex-direction: column;
+                gap: 16px;
+            ">
+                <div style="
+                    width: 255px;
+                    height: 56px;
+                    font-weight: 700;
+                    font-size: 24px;
+                    line-height: 28px;
+                    letter-spacing: 0;
+                    color: #262626;
+                ">
+                    Increase upgrades and selections
+                </div>
+                <div style="
+                    width: 342px;
+                    height: 112px;
+                    font-weight: 400;
+                    font-size: 18px;
+                    line-height: 28px;
+                    letter-spacing: 0px;
+                    color: #303030;
+                ">
+                    When homebuyers can see upgraded finishes, fixtures and options in context, they are more confident in their selections, leading to higher upgrade spend.
+                </div>
+            </div>
+        </div>
+         <div style="
+            width: 364px;
+            height: 508px;
+            display: flex;
+            flex-direction: column;
+            gap: 30px;
+        ">
+            <img src="pages/homepage/media/img5.png" style="
+                width: 364px;
+                height: 294px;
+                opacity: 1;
+            ">
+            <div style="
+                display: flex;
+                flex-direction: column;
+                gap: 16px;
+            ">
+                <div style="
+                    width: 364px;
+                    height: 56px;
+                    font-weight: 700;
+                    font-size: 24px;
+                    line-height: 28px;
+                    letter-spacing: 0;
+                    color: #262626;
+                ">
+                    A more bespoke and efficient process for your team
+                </div>
+                <div style="
+                    height: 112px;
+                    font-weight: 400;
+                    font-size: 18px;
+                    line-height: 28px;
+                    letter-spacing: 0px;
+                    color: #303030;
+                ">
+                    Optimize your engagement with homebuyers via personalised brochures and the use of detailed analytics, helping streamline sales and design studio workflows.
+                </div>
+            </div>
+        </div>
+    </div>
+    <div style="
+        width: 640px;
+        height: 84px;
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 28px;
+        letter-spacing: 0px;
+        text-align: center;
+    ">
+        Away Digital Home creates Photorealistic renders, cinematic videos, immersive 360° tours and personalized brochures support every stage of the homebuilding journey, from first touchpoint through to final selections.
+    </div>
+</section>
